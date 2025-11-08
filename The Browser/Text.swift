987b6422ed6ce550baaf1bbc31704679417be8ac
Alec Wilson <hi@judes.club>
2025-11-08 13:41:05 -0500

    initial comit and setup

---
+++ The Browser/Text.swift
@@ -0,0 +1,18 @@
+//
+//  The_BrowserApp.swift
+//  The Browser
+//
+//  Created by Jude Wilson on 11/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TheBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("the Browser")
+        }
+    }
+}
